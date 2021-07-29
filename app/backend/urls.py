957d15f6c0e3a4e9846cc6342411a3fcf05115de
Zunaid Amin Enan <zunaid321@gmail.com>
2021-07-30 05:47:30 +0600

    Backend Change

--- app/backend/urls.py
+++ app/backend/urls.py
@@ -1,4 +1,4 @@
-"""app URL Configuration
+"""backend URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -14,16 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.conf.urls.static import static
+from django.urls import path, include
 from django.conf import settings
+from django.conf.urls.static import static
+from .views import *
 
 urlpatterns = [
+    path('', defaultDirect),
     path('admin/', admin.site.urls),
+    path('api/', include('base.urls')),
+    path('api-auth/', include('rest_framework.urls'))
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT,
-    )
+urlpatterns+= static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
